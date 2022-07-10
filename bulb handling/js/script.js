c1f598f4a46a81ab705cafe9e4de19a564acb265
Nima Noorali <96994243+mynameisNN2@users.noreply.github.com>
2022-07-10 13:22:11 +0430

    Add files via upload

---
+++ bulb handling/js/script.js
@@ -0,0 +1,16 @@
+﻿// get the elements
+let btnElem = document.getElementById("btn");
+let imgElem = document.getElementById("bulb");
+let flag = false;
+
+function turnOffOrOn() {
+  if (flag) {
+    imgElem.setAttribute("src", "pics/bulboff.gif");
+    btnElem.innerHTML = "Turn on";
+    flag = false;
+  } else {
+    imgElem.setAttribute("src", "pics/bulbon.gif");
+    btnElem.innerHTML = "Turn off";
+    flag = true;
+  }
+}
