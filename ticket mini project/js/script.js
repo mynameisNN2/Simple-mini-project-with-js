007ac6483a1afef29d4bbac7db5fe0d490a2a0cf
Nima Noorali <96994243+mynameisNN2@users.noreply.github.com>
2022-07-11 15:38:09 +0430

    Add files via upload

---
+++ ticket mini project/js/script.js
@@ -0,0 +1,23 @@
+﻿let countrySelectBox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+
+let countrydata = {
+  Iran: ["Tehran", "Ahwaz", "Esfahan", "Tabriz", "Mashhad"],
+  Turkey: ["Istanbul", "Ezmir", "Ankara"],
+  US: ["New York", "Chicago", "Los Angles"],
+};
+
+countrySelectBox.addEventListener("change", function () {
+  if (countrySelectBox.value === "Please Select") {
+    citySelect.innerHTML = "";
+    citySelect.innerHTML += "<option>Select City ...</option>";
+  } else {
+    let maincountryName = countrySelectBox.value;
+    let maincounrtycities = countrydata[maincountryName];
+
+    citySelect.innerHTML = "";
+    maincounrtycities.forEach(function (city) {
+      citySelect.innerHTML += "<option>" + city + "</option>";
+    });
+  }
+});
