76e7eebcb732c313ab328ef17ebd80aa72b3aff2
Nima Noorali <96994243+mynameisNN2@users.noreply.github.com>
2022-07-11 13:54:37 +0430

    Add files via upload

---
+++ login form validation/js/script.js
@@ -0,0 +1,23 @@
+﻿// get the inputs and button
+
+let usernameElem = document.querySelector(".username");
+let passwordElem = document.querySelector(".password");
+let modalElem = document.querySelector(".modal");
+
+function dataValidation() {
+  let userValue = usernameElem.Value;
+  let passvalue = passwordElem.value;
+
+  if (userValue.lenght < 12 || passvalue.lenght < 8) {
+    modalElem.style.display = "inline";
+    modalElem.style.background = "red";
+    modalElem.innerHTML = "اطلاعات را به درستی وارد نمایید";
+  } else {
+    modalElem.style.display = "inline";
+    modalElem.style.background = "green";
+    modalElem.innerHTML = "اطلاعات با موفقیت ثبت شد";
+  }
+  setTimeout(function(){
+    modalElem.style.display = "none";
+  }, 3000);
+}
